71e013a9434af54c678e87740945fe5ad33ba5ce
Pablo Duran <pabloagduran@gmail.com>
2023-12-21 15:53:45 -0300

    f

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -28,18 +28,6 @@ const Productos = () => {
         ))}
       </div>
 
-      <div>
-        <h2>Carrito</h2>
-        {cart.length === 0 ? (
-          <p>El carrito está vacío</p>
-        ) : (
-          <ul>
-            {cart.map((item, index) => (
-              <li key={index}>{item.name} - ${item.price}</li>
-            ))}
-          </ul>
-        )}
-      </div>
     </div>
   );
 };
